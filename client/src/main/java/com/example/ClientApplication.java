d342f8829c08aca33bfd0169c93b199fff2ac2ca
Florian Garcia <florian.garcia@ext.cdiscount.com>
2016-11-18 18:05:09 +0100

    Zuul version

--- client/src/main/java/com/example/ClientApplication.java
+++ client/src/main/java/com/example/ClientApplication.java
@@ -4,35 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableFeignClients
 @RestController
 @SpringBootApplication
+@EnableZuulProxy
 public class ClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
-    }
-
-    @FeignClient(value = "sleep")
-    interface SleepClient {
-
-        @RequestMapping(value = "/sleep")
-        String sleep();
-    }
-
-    private SleepClient sleepClient;
-
-    public ClientApplication(SleepClient sleepClient) {
-        this.sleepClient = sleepClient;
-    }
-
-    @RequestMapping(value = "/test")
-    public String getGoogle() {
-        // If no timeout
-        return sleepClient.sleep();
     }
 
     @RequestMapping(value = "/sleep")
